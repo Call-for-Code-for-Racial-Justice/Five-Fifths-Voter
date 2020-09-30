058e49ee49396c2dde6f6b1476f64a83b4dd5f26
Syd Bailey <sydabailey@gmail.com>
2020-09-30 12:38:03 -0500

    (feat)ui-shell-global-header: Making ui shell mobile friendly issue 17 (#23)
    * (feat)global-header: added mobile menu button
    
    * feat(mobile-menu): added functionality for mobile menu to open/close upon click
    
    Co-authored-by: syd bailey <syd@syds-MBP.attlocal.net>

--- src/components/AppHeader/AppHeader.vue
+++ src/components/AppHeader/AppHeader.vue
@@ -1,26 +1,57 @@
 <template>
-  <cv-header aria-label="Carbon tutorial">
-    <cv-skip-to-content href="#main-content"
-      >Skip to content</cv-skip-to-content
-    >
+  <cv-header aria-label="Carbon header">
+    <cv-header-menu-button aria-label="Header menu" aria-controls="side-nav" />
+    <cv-skip-to-content href="#main-content">
+      Skip to content
+    </cv-skip-to-content>
     <cv-header-name to="/">embRACEtheVOTE</cv-header-name>
-    <cv-header-menu-item :to="{ name: 'why-page' }"
-      >Why Vote</cv-header-menu-item
-    >
-    <cv-header-menu-item :to="{ name: 'journey-page' }"
-      >Voter Journey</cv-header-menu-item
-    >
-    <cv-header-menu-item :to="{ name: 'connect-page' }"
-      >Get Connected</cv-header-menu-item
-    >
-    <cv-header-menu-item :to="{ name: 'support-page' }"
-      >Voter Support</cv-header-menu-item
-    >
+    <cv-header-nav aria-label="Carbon nav">
+      <cv-header-menu-item :to="{ name: 'why-page' }"
+        >Why Vote</cv-header-menu-item
+      >
+      <cv-header-menu-item :to="{ name: 'journey-page' }"
+        >Voter Journey</cv-header-menu-item
+      >
+      <cv-header-menu-item :to="{ name: 'connect-page' }"
+        >Get Connected</cv-header-menu-item
+      >
+      <cv-header-menu-item :to="{ name: 'support-page' }"
+        >Voter Support</cv-header-menu-item
+      >
+    </cv-header-nav>
+    <template v-slot:left-panels v-if="areLeftPanels">
+      <cv-side-nav id="side-nav" fixed>
+        <cv-side-nav-items>
+          <cv-header-side-nav-items>
+            <cv-header-menu-item :to="{ name: 'why-page' }"
+              >Why Vote</cv-header-menu-item
+            >
+            <cv-header-menu-item :to="{ name: 'journey-page' }"
+              >Voter Journey</cv-header-menu-item
+            >
+            <cv-header-menu-item :to="{ name: 'connect-page' }"
+              >Get Connected</cv-header-menu-item
+            >
+            <cv-header-menu-item :to="{ name: 'support-page' }"
+              >Voter Support</cv-header-menu-item
+            >
+          </cv-header-side-nav-items>
+        </cv-side-nav-items>
+      </cv-side-nav>
+    </template>
+    <template v-slot:right-panels v-if="areRightPanels"> </template>
   </cv-header>
 </template>
 
 <script>
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  data() {
+    return {
+      areLeftPanels() {
+        return false;
+      }
+    };
+  }
 };
 </script>
