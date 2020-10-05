25e8609bc06aa46277600205c44dbfee20ba8807
Shrey <shanand@redhat.com>
2020-10-05 18:35:28 -0400

    Add features to twitter service
    - Add IBM Watson sentiment, emotion, concept, and tone detection
    - Show only the recent tweets posted by the user and remove their RTs and replies
    - Add capability for generating word clouds for a condensed representation of the tweets

--- services/twitter/Embrace_Challenge.py
+++ services/twitter/Embrace_Challenge.py
@@ -7,6 +7,13 @@ import re
 import time
 import os
 import sys
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features,\
+SentimentOptions, EmotionOptions, ConceptsOptions
+from ibm_watson import ToneAnalyzerV3
+from wordcloud import WordCloud, STOPWORDS
 
 ## Enter your twitter credentials here in .env file or set them as environment variables
 ## To be fixed, see https://github.com/embrace-call-for-code/embrace-the-vote/issues/22
@@ -15,10 +22,34 @@ csecret=os.environ.get('NODE_TWITTER_API_SECRET_KEY')
 atoken=os.environ.get('NODE_TWITTER_ACCESS_TOKEN')
 asecret=os.environ.get('NODE_TWITTER_ACCESS_TOKEN_SECRET')
 
+## Enter your IBM Watson NLU api and tone analyzer credentials here in .env file or set them as environment variables
+nlu_api_key = os.environ.get('NODE_NLU_API_KEY')
+nlu_api_url = os.environ.get('NODE_NLU_API_URL')
+
+tone_analyzer_api_key = os.environ.get('NODE_TA_API_KEY')
+tone_analyzer_api_url = os.environ.get('NODE_TA_API_URL')
+
 ## Connect to twitter API
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 api = tweepy.API(auth)
+
+## Connect to IBM Watson NLU API
+nlu_authenticator = IAMAuthenticator(nlu_api_key)
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2020-08-01',
+    authenticator=nlu_authenticator
+)
+natural_language_understanding.set_service_url(nlu_api_url)
+
+## Connect to IBM Watson tone analyzer API
+ta_authenticator = IAMAuthenticator(tone_analyzer_api_key)
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    authenticator=ta_authenticator
+)
+tone_analyzer.set_service_url(tone_analyzer_api_url)
+
 
 ## Retrieve query search name
 ## and set number of tweets to be shown
@@ -27,72 +58,103 @@ count = 25
 
 def get_tweets(search_name, topk):
     """
-    This function first finds screen_name (~twitter ID) that matches best with the search_name.
-    Then it returns full topk recent tweets from that user timeline.
+    This function first finds screen_name (~twitter ID) that mathces best with the search_name.
+    Then it returns topk recent tweets (excluding retweets and replies) from that user timeline.
     """
     ## Get screen name
     screen_name = api.search_users(q=search_name, count=1)[0].screen_name
-    ## Get extended topk tweets for that screenname
-    raw_output = tweepy.Cursor(api.user_timeline, screen_name=screen_name, tweet_mode="extended").items(topk)
+    ## Get extended topk tweets for that screenname excluding retweets and replies
+    raw_output = tweepy.Cursor(api.user_timeline,
+                               screen_name=screen_name,
+                               tweet_mode="extended",
+                               include_rts=False,
+                               exclude_replies=True).items(topk)
     tweets = [tweet.full_text for tweet in raw_output]
     return screen_name, tweets
 
-def get_sentiments_subjectivity(tweets):
+def get_nlu_tone_analysis(tweets):
     """
     This function takes input as a list of tweets and
-    returns the sentiment (Positive, Neutral or Negative) and
-    subjectivity (Subjective, Neutral, Objective)
-    )
+    returns their sentiment (Positive, Neutral or Negative),
+    concepts (high level concepts or ideas),
+    emotions (anger, disgust, fear, joy, or sadness),
+    and tones (emotional and language tone)
     """
     result = list()
-    
     for tweet in tweets:
-        ## Remove URLs
+	## Remove URLs
         tweet_cleaned = re.sub(r'http\S+', '', tweet)
-        
-        ## Get sentiments and subjectivity scores
-        tweet_blob = TextBlob(tweet_cleaned)
-        sentiment_polarity =  tweet_blob.sentiment.polarity
-        tweet_subjectivity =  tweet_blob.sentiment.subjectivity
-        
-        ## Convert scores to labels
-        if sentiment_polarity > 0:
-            sentiment = "Positive"
-        elif sentiment_polarity == 0:
-            sentiment = "Neutral"
+	
+        if tweet_cleaned:
+	    ## Call NLU API
+            nlu_analysis = natural_language_understanding.analyze(
+                text=tweet_cleaned, language='en',
+                      features=Features(
+                      concepts=ConceptsOptions(limit=2),
+                      sentiment=SentimentOptions(),
+                      emotion=EmotionOptions())).get_result()
+
+            concepts = ', '.join([concept['text'] for concept in nlu_analysis['concepts']])
+            sentiment = nlu_analysis['sentiment']['document']['label']
+            emotions = nlu_analysis['emotion']['document']['emotion']
+            dominant_emotion = max(emotions, key=emotions.get)
+	    
+	    ## Call tone analyzer API
+            tone_analysis = tone_analyzer.tone(
+                    {'text': tweet_cleaned},
+                    content_type='text'
+            ).get_result()
+
+            tones = ', '.join([tone['tone_name'] for tone in tone_analysis['document_tone']['tones']])
+	    
+	    ## Create result table
+            temp_dic = {'tweet':tweet, 'sentiment':sentiment, "emotion":dominant_emotion,
+                  'concepts':concepts, 'tones':tones}
         else:
-            sentiment = "Negative"
-            
-        if tweet_subjectivity > 0:
-            subjectivity = "Subjective"
-        elif tweet_subjectivity == 0:
-            subjectivity = "Neutral"
-        else:
-            subjectivity = "Objective"
-        
-        dic = {'tweet':tweet, 'sentiment':sentiment, "subjectivity":subjectivity}            
-        result.append(dic)
+            temp_dic = {'tweet':tweet, 'sentiment':'', "emotion":'',
+                  'concepts':'', 'tones':''}
+        result.append(temp_dic)
         
     return result
 
-_, statuses = get_tweets(screen_name, topk=count)
+## Code for dynamically generating wordcloud
+## This has to be called from the front end or the main
+## function of this script and 
+## the plot has to be saved at a path for the
+## front end to collect it and display.
+def clean_tweets_for_wordcloud(tweets):
+    ## Join all tweets
+    tweets = ' '.join(tweets)
+    ## remove URLs
+    tweets = re.sub(r'http\S+', '', tweets)
+    return tweets
 
-statuses_with_labels = get_sentiments_subjectivity(statuses)
+def plot_cloud(wordcloud):
+    plt.figure(figsize=(40, 30))
+    plt.imshow(wordcloud) 
+    plt.axis("off")
+    plt.savefig("PATH")
 
+if __name__ == "__main__":
 
-## To be fixed: This should return a json output instead of dom element, see 
-## https://github.com/embrace-call-for-code/embrace-the-vote/issues/13
-print ('<ul class="cv-list bx--list--unordered">')
-for status in statuses_with_labels:
-		list_class = '<li class="cv-list-item bx--list__item">'
-		tweet = status['tweet'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
-		sentiment = status['sentiment'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
-		subjectivity = status['subjectivity'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
-		print(list_class,tweet,"</li>")
-		print(list_class,sentiment,subjectivity,"</li>")
+		_, statuses = get_tweets(screen_name, topk=count)
 
-print ('</ul>')
-sys.stdout.flush()
+		statuses_with_labels = get_nlu_tone_analysis(statuses)
 
-exit(0)
+		## To be fixed: This should return a json output instead of dom element, see 
+		## https://github.com/embrace-call-for-code/embrace-the-vote/issues/13
+		## For now, the result table is printed as dom elements
+		print ('<ul class="cv-list bx--list--unordered">')
+		for status in statuses_with_labels:
+				list_class = '<li class="cv-list-item bx--list__item">'
+				tweet = status['tweet'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
+				sentiment = "Sentiment-" + status['sentiment'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
+				concepts = "Concepts-" + status['concepts'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
+				emotion = "Emotion-" + status['emotion'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
+				tones = "Tones-" + status['tones'].encode(encoding='ASCII',errors='ignore').decode('ASCII')
+				print(list_class,tweet,"</li>")
+				print(list_class,sentiment,emotion,concepts,tones,"</li>")
+		print ('</ul>')
+		sys.stdout.flush()
 
+		exit(0)
