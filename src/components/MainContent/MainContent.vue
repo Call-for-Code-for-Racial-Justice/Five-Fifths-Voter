f5876ee5db94656ed424e0bf32be29b42871265f
syd bailey <syd@syds-MacBook-Pro.local>
2020-10-07 18:08:26 -0500

    fix(main-content): quick fix for responsiveness

--- src/components/MainContent/MainContent.vue
+++ src/components/MainContent/MainContent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bx--grid">
     <div class="bx--row">
-      <div class="bx--col">
+      <div class="bx--col-sm-12 bx--col-lg-6">
         <slot name="content" />
       </div>
-      <div class="bx--col">
+      <div class="bx--col-sm-12 bx--col-lg-6">
         <slot name="image" />
       </div>
     </div>
