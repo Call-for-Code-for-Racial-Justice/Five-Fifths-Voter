4a2537dc6a0ed55f285096e02eb5ba7964f71277
Syd Bailey <sydabailey@gmail.com>
2020-10-09 13:09:31 -0500

    feat(connect-page): updated page layout (#49)
    * feat(why-vote): updated page layout
    
    * feat(connect-page): updated page layout
    
    Co-authored-by: syd bailey <syd@syds-MacBook-Pro.local>

--- src/views/ConnectPage/ConnectPage.vue
+++ src/views/ConnectPage/ConnectPage.vue
@@ -1,42 +1,54 @@
 <template>
-  <cv-content id="#main-content">
-    <div class="bx--grid bx--grid--full-width landing-page">
-      <div class="bx--row landing-page__r2">
-        <div class="bx--col-lg-16">
-          <section class="main main--register">
-            <aside class="content--register">
-              <h1 class="heading--lg">vote social</h1>
-              <h4 class="heading--sm">take a friend to vote</h4>
-              <p>
-                <cv-link
-                  href="https://www.facebook.com/gpa/best-practices/local-government"
-                  >Facebook</cv-link
-                ><br />
-                <cv-link
-                  href="https://www.instagram.com/explore/tags/election/?hl=en"
-                  >Instagram #Election</cv-link
-                ><br />
-                <cv-link
-                  href="https://twitter.com/search?q=%23election&src=typed_query"
-                  >Twitter #Election</cv-link
-                ><br />
-                <br />
-              </p>
-              <TwitterChatter />
-            </aside>
-          </section>
-        </div>
-      </div>
+  <PageLayout childData="mother-daughter-hero.jpeg">
+    <div class="page__details">
+      <h2 class="page__title">Vote social</h2>
     </div>
-  </cv-content>
+    <MainContent>
+      <template v-slot:content>
+        <aside class="aside">
+          <div class="aside__container--text">
+            <h2 class="aside__header">Resources are available to help you</h2>
+            <div class="aside__container--links">
+              <a
+                href="https://www.facebook.com/gpa/best-practices/local-government"
+                ><img
+                  style="fill: #fff"
+                  src="@/assets/logo--facebook.svg"
+                  alt="Facebook Logo"/></a
+              ><br />
+              <!-- <a href="https://www.instagram.com/explore/tags/election/?hl=en"
+                >Instagram #Election</a
+              ><br />
+              <a href="https://twitter.com/search?q=%23election&src=typed_query"
+                >Twitter #Election</a
+              ><br /> -->
+              <br />
+            </div>
+            <TwitterChatter />
+          </div>
+        </aside>
+      </template>
+      <template v-slot:image>
+        <aside class="aside__container--img">
+          <img
+            class="aside__image"
+            src="../../assets/connect-page-friends-image-1.jpg"
+            alt="3 friends with sunglasses smile into the camera while hugging"
+          />
+        </aside>
+      </template>
+    </MainContent>
+  </PageLayout>
 </template>
 
 <script>
 import TwitterChatter from '../../components/TwitterChatter';
+import PageLayout from '../../components/PageLayout';
+import MainContent from '../../components/MainContent';
 
 export default {
   name: 'connect',
-  components: { TwitterChatter },
+  components: { TwitterChatter, PageLayout, MainContent },
   data() {
     return {};
   },
