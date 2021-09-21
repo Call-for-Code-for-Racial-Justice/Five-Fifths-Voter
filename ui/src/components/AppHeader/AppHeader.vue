7c373ebee95df502af1a7101309989e75985e5e5
John Fiewor <johnfiewor@gmail.com>
2021-09-21 23:23:45 +0100

    fix #209
    Signed-off-by: John Fiewor <johnfiewor@gmail.com>

--- ui/src/components/AppHeader/AppHeader.vue
+++ ui/src/components/AppHeader/AppHeader.vue
@@ -7,7 +7,7 @@
     <cv-header-name :to="{ name: 'landing-page' }" prefix="">
       Five Fifths Voter
     </cv-header-name>
-    <cv-header-nav aria-label="Carbon nav">
+    <cv-header-nav aria-label="main menu">
       <cv-header-menu-item :to="{ name: 'why-page' }">
         {{ $t('appHeaderWhyVote') }}
       </cv-header-menu-item>
@@ -22,29 +22,9 @@
       </cv-header-menu-item>
     </cv-header-nav>
     <template v-slot:header-global>
-      <cv-header-global-action aria-label="User avatar" aria-controls="user-panel">
+      <cv-header-global-action aria-label="User avatar">
         <UserAvatar20 />
       </cv-header-global-action>
-    </template>
-    <template v-slot:left-panels v-if="areLeftPanels">
-      <cv-side-nav id="side-nav" fixed>
-        <cv-side-nav-items>
-          <cv-header-side-nav-items>
-            <cv-header-menu-item :to="{ name: 'why-page' }">
-              Why Vote
-            </cv-header-menu-item>
-            <cv-header-menu-item :to="{ name: 'journey-page' }">
-              Voter Journey
-            </cv-header-menu-item>
-            <cv-header-menu-item :to="{ name: 'connect-page' }">
-              Get Connected
-            </cv-header-menu-item>
-            <cv-header-menu-item :to="{ name: 'support-page' }">
-              Voter Support
-            </cv-header-menu-item>
-          </cv-header-side-nav-items>
-        </cv-side-nav-items>
-      </cv-side-nav>
     </template>
   </cv-header>
 </template>
@@ -56,10 +36,7 @@ export default {
   name: 'AppHeader',
   components: { UserAvatar20 },
   data() {
-    return {
-      areLeftPanels() {
-        return false;
-      }
+    return {}
     };
   }
 };
