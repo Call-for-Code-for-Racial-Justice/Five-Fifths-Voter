9480c90dd3d3bb429224cbc07ba295c770feaf6a
Ariana Isabel Hlavaty <arianahl@uk.ibm.com>
2021-10-03 18:41:16 +0100

    ui: show error on invalid page
    Signed-off-by: Ariana Isabel Hlavaty <arianahl@uk.ibm.com>

--- ui/src/router.js
+++ ui/src/router.js
@@ -42,6 +42,11 @@ export default new Router({
       // this generates a separate chunk (reg-page.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "reg-page" */ './views/WhyVotePage')
+    },
+    {
+      // will match everything
+      path: '*',
+      component: () => import(/* webpackChunkName: "reg-page" */ './views/NotFoundPage')
     }
   ]
 });
---
+++ ui/src/views/NotFoundPage/NotFoundPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <cv-content id="#main-content">
+    <div class="page__details">
+      <h2 class="aside__header">Page Not Found</h2>
+    </div>
+    <div class="bx--grid bx--grid--full-width landing-page">
+      <div class="bx--row landing-page__r2">
+        <div class="bx--col-lg-16">
+          <section>
+            <aside class="content--register">
+              <p>Redirecting...</p>
+            </aside>
+          </section>
+        </div>
+      </div>
+    </div>
+  </cv-content>
+</template>
+
+<script>
+import router from '../../router';
+export default {
+  name: 'notfound',
+  components: {},
+  data() {
+    return {};
+  },
+  created() {
+    setTimeout(() => router.push('/'), 2000);
+  },
+  methods: {}
+};
+</script>
