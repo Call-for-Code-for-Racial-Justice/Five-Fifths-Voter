5e6d3392e0b0fb7eb89a94814099109c21560161
Upkar Lidder <ulidder@us.ibm.com>
2021-11-18 11:09:44 -0800

    adding android source code created with bubblewrap
    Signed-off-by: Upkar Lidder <ulidder@us.ibm.com>

---
+++ android/app/src/main/java/com/fivefifthsvoter/www/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.fivefifthsvoter.www.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
