2df519b1d41600156201fd0c4e08059511b6dbf3
David Nixon <dwnixon@gmail.com>
2024-03-02 17:59:44 -0500

    chore: create components for the values section

---
+++ app/components/Home/Values/Section.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="our-values" class="h-screen w-full bg-ff-blue-01 pt-[3rem]">
+    <div class="ml-auto mr-auto w-[99%] md:w-[90%]">
+      <h1 class="p-10 text-5xl text-ff-blue-03 md:text-6xl">Our values</h1>
+      <div class="m-2 max-w-[1300px] space-y-2">
+        <home-values-card
+          :name="$t('landingPageEdu')"
+          image="educate"
+          :main="$t('landingPageEduDesc', ['Five Fifths Voter'])"
+          :quote="$t('quoteFrancisBacon')"
+          author="Francis Bacon"
+        />
+        <home-values-card
+          :name="$t('landingPageEmpower')"
+          image="empower"
+          :main="$t('landingPageEmpowerDesc', ['Five Fifths Voter'])"
+          :quote="$t('quoteCharlotteBronte')"
+          author="Charlotte Brontë"
+        />
+        <home-values-card
+          :name="$t('landingPageEnable')"
+          image="enable"
+          :main="$t('landingPageEnableDesc', ['Five Fifths Voter'])"
+          :quote="$t('quotePaulRepetto')"
+          author="Paul Repetto"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineOptions({
+  name: "HomeValuesSection",
+});
+const expandedCard = ref("");
+provide("expanded-card", expandedCard);
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <home-header />
-    <section id="top" class="w-full h-dvh mt-[2rem]">
+    <section id="top" class="mt-[2rem] h-dvh w-full">
       <div
-        class="w-full h-[calc(100%-320px-2rem)] bg-ff-purple-01 bg-repeat-x bg-[url(@/assets/images/silhouette-tile.png)] bg-cover md:bg-contain"
+        class="h-[calc(100%-320px-2rem)] w-full bg-ff-purple-01
+          bg-[url(@/assets/images/silhouette-tile.png)] bg-cover bg-repeat-x md:bg-contain"
       >
         <div class="p-8">
           <img
@@ -11,11 +12,11 @@
             src="@/assets/images/five-fifths-voter.svg"
           />
         </div>
-        <div class="text-white p-3 md:p-14 h-full w-full">
+        <div class="h-full w-full p-3 text-white md:p-14">
           <h1 class="text-4xl md:text-6xl">
             Overcoming barriers<br />Amplifying voices
           </h1>
-          <div class="text-2xl md:text-2xl pt-10 md:pt-8 md:max-w-96">
+          <div class="pt-10 text-2xl md:max-w-96 md:pt-8 md:text-2xl">
             Together, we can change the narrative of historically
             disenfranchised people in our democratic process.
           </div>
@@ -25,32 +26,35 @@
     </section>
     <section
       id="our-mission"
-      class="w-full h-dvh bg-ff-white-01 flex justify-between justify-items-center gap-2"
+      class="flex h-dvh w-full justify-between justify-items-center gap-2 bg-ff-white-01"
     >
       <div
-        class="hidden m:block h-full w-[20%] bg-[url(@/assets/images/oval-purple-left.svg)] bg-right bg-no-repeat"
+        class="m:block hidden h-full w-[20%] bg-[url(@/assets/images/oval-purple-left.svg)]
+          bg-right bg-no-repeat"
       ></div>
       <div
-        class="h-full bg-[url(@/assets/images/oval-purple.svg)] bg-cover md:bg-contain bg-center bg-no-repeat flex items-center justify-items-center"
+        class="flex h-full items-center justify-items-center
+          bg-[url(@/assets/images/oval-purple.svg)] bg-cover bg-center bg-no-repeat
+          md:bg-contain"
       >
-        <div class="w-full flex flex-col items-center justify-around">
+        <div class="flex w-full flex-col items-center justify-around">
           <h1
-            class="text-ff-orange-01 text-6xl md:text-8xl text-center mt-10 md:mt-0 p-4"
+            class="mt-10 p-4 text-center text-6xl text-ff-orange-01 md:mt-0 md:text-8xl"
           >
             Our mission
           </h1>
-          <p class="text-white text-1xl md:text-2xl text-center p-4 w-[80%]">
+          <p class="text-1xl w-[80%] p-4 text-center text-white md:text-2xl">
             With Five Fifths Voter, we help put the power back in your hands.
             Our platform provides a personalized experience with useful tools to
             help marginalized voters overcome voting process roadblocks.
           </p>
-          <p class="text-white text-1xl md:text-2xl text-center p-4 w-[80%]">
+          <p class="text-1xl w-[80%] p-4 text-center text-white md:text-2xl">
             Five Fifths Voter takes into account various voting journeys, such
             as first-time voters, veterans, minorities, legally disabled, the
             elderly, or those with felony convictions.
           </p>
           <button
-            class="bg-ff-purple-01 text-2xl p-3 text-white"
+            class="bg-ff-purple-01 p-3 text-2xl text-white"
             @click="router.replace({ name: 'journey' })"
           >
             Your Voter Journey
@@ -59,50 +63,11 @@
         </div>
       </div>
       <div
-        class="hidden m:block h-full w-[20%] bg-[url(@/assets/images/oval-purple-right.svg)] bg-left bg-no-repeat"
+        class="m:block hidden h-full w-[20%] bg-[url(@/assets/images/oval-purple-right.svg)]
+          bg-left bg-no-repeat"
       ></div>
     </section>
-    <section id="our-values" class="w-full h-screen bg-ff-blue-01 pt-[3rem]">
-      <div class="w-[99%] md:w-[90%] mr-auto ml-auto">
-        <h1 class="text-ff-blue-03 text-5xl md:text-6xl p-10">Our values</h1>
-
-        <div class="m-2 space-y-2 max-w-[1300px]">
-          <home-values-card name="Educate" image="educate">
-            <template #main>
-              Five Fifths Voter will educate disenfranchised voters with voting
-              guidance and connection to up&#x2011;to&#x2011;date tools
-              necessary to complete the voting process.
-            </template>
-            <template #quote> Knowledge is power </template>
-            <template #author> Francis Bacon </template>
-          </home-values-card>
-          <home-values-card name="Empower" image="empower">
-            <template #main>
-              Five Fifths Voter will help generate solidarity for
-              disenfranchised voting communities and leverage social media
-              platforms for easy access to candidate information.
-            </template>
-            <template #quote>
-              I am no bird; and no net ensnares me: I am a free human being with
-              an independent will.
-            </template>
-            <template #author> Charlotte Brontë </template>
-          </home-values-card>
-          <home-values-card name="Enable" image="enable">
-            <template #main>
-              Five Fifths Voter enables users with information to get to polling
-              stations in safe and efficient ways and will strive to help remove
-              roadblocks disenfranchised voters encounter when voting.
-            </template>
-            <template #quote>
-              Enablement is the key to closing the gap between intention and
-              action.
-            </template>
-            <template #author> Paul Repetto </template>
-          </home-values-card>
-        </div>
-      </div>
-    </section>
+    <home-values-section />
   </div>
 </template>
 
