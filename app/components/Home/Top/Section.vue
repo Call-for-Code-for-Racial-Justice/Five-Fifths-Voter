854c197e7ca88d5ca1363651c20da35c6aced3f5
David Nixon <dwnixon@gmail.com>
2024-03-03 16:43:11 -0500

    chore: add component for hom/top section

---
+++ app/components/Home/Top/Section.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="top" class="mt-[2rem] h-dvh w-full">
+    <div
+      class="h-[calc(100%-320px-2rem)] w-full bg-ff-purple-01
+        bg-[url(@/assets/images/silhouette-tile.png)] bg-cover bg-repeat-x md:bg-contain"
+    >
+      <div class="p-8">
+        <img
+          alt="Five Fifths voter"
+          src="@/assets/images/five-fifths-voter.svg"
+        />
+      </div>
+      <div class="h-full w-full p-3 text-white md:p-14">
+        <h1 class="text-4xl md:text-6xl">
+          {{ $t("landingTopTagline") }}
+          <mark-down :content="$t('landingTopTagline')" />
+        </h1>
+        <div class="pt-10 text-2xl md:max-w-96 md:pt-8 md:text-2xl">
+          {{ $t("landingTopDesc") }}
+        </div>
+      </div>
+    </div>
+    <home-countdown />
+  </section>
+</template>
+
+<script setup>
+defineOptions({
+  name: "HomeTopSection",
+});
+</script>
+
+<style scoped lang="scss"></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,29 +1,8 @@
 <template>
   <div>
     <home-header />
-    <section id="top" class="mt-[2rem] h-dvh w-full">
-      <div
-        class="h-[calc(100%-320px-2rem)] w-full bg-ff-purple-01
-          bg-[url(@/assets/images/silhouette-tile.png)] bg-cover bg-repeat-x md:bg-contain"
-      >
-        <div class="p-8">
-          <img
-            alt="Five Fifths voter"
-            src="@/assets/images/five-fifths-voter.svg"
-          />
-        </div>
-        <div class="h-full w-full p-3 text-white md:p-14">
-          <h1 class="text-4xl md:text-6xl">
-            Overcoming barriers<br />Amplifying voices
-          </h1>
-          <div class="pt-10 text-2xl md:max-w-96 md:pt-8 md:text-2xl">
-            Together, we can change the narrative of historically
-            disenfranchised people in our democratic process.
-          </div>
-        </div>
-      </div>
-      <home-countdown />
-    </section>
+    <home-top-section />
+    <!-- TODO: Move to component -->
     <section
       id="our-mission"
       class="flex h-dvh w-full justify-between justify-items-center gap-2 bg-ff-white-01"
