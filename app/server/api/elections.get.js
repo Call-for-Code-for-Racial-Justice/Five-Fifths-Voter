04acaf06483d946f0a7e289dffbf5388f3eee26b
David Nixon <david_nixon@us.ibm.com>
2024-09-12 13:20:32 -0400

    fix: donot show test election

--- app/server/api/elections.get.js
+++ app/server/api/elections.get.js
@@ -1,5 +1,10 @@
 import { useWinstonLogger } from "~/utils/logging";
 
+function shouldInvalidateCache(event) {
+  const query = getQuery(event);
+  return query?.invalidate === "true";
+}
+
 export default defineCachedEventHandler(
   (event) => {
     const logger = useWinstonLogger("civics-api");
@@ -9,5 +14,9 @@ export default defineCachedEventHandler(
       query: { ...event.context.civics.query },
     });
   },
-  { maxAge: 60 * 60 * 24 },
+  {
+    maxAge: 60 * 60 * 24,
+    shouldInvalidateCache,
+    getKey: () => "civic:elections",
+  },
 );
